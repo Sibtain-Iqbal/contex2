d0ba1e467aeb42810320ac1d8568501801d5ac29
Sibtain Iqbal <sibtainiqbal8844@gmail.com>
2024-01-11 12:16:22 +0500

    contetx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeContext } from './Context/theme'
 
 function App() {
 
+  const  [ themeMode , setthemeMode] = useState("light")
+const DarkTheme =()=>{
+  setthemeMode("light")
+}
+const lightTheme =()=>{
+  setthemeMode("dark")
+}
+
+// actual change in theme 
+useEffect(()=>{
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(themeMode)
+},[themeMode])
+
   return (
+    <ThemeProvider value={{Theme,DarkTheme,lightTheme}} >
+       
+ 
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -17,6 +34,8 @@ function App() {
         </div>
     </div>
 </div>
+  </ThemeProvider>
+
   )
 }
 
